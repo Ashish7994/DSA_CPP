5349326cd11cd06c3a68424b2525ecd5025ba8fc
Ashish7994 <ashish.admn@gmail.com>
2024-11-10 21:50:34 +0530

    max sum subarray

--- Array/maxsum_subarray_sum.cpp
+++ Array/maxsum_subarray_sum.cpp
@@ -16,13 +16,27 @@ class solution{
         return maxsum;
     }
 
+    int maxsum_subarray(vector<int>& arr)
+    {
+        int currsum = 0;
+        int maxsum = INT16_MIN;
+        for(int val:arr)
+        {
+            currsum += val;
+            maxsum = max(maxsum, currsum);
+        }
+        return maxsum;
+    }
+
 };
 
 int main(){
-    int arr[] = {0,1,2,3,4,5,-6,7};
+    int arr[] = {0,1,2,-3,4,5,-6,7};
     int n = sizeof(arr)/sizeof(arr[0]);
     solution maxsum;
-
     cout << "max sum: " << maxsum.maxsum_subarray(arr,n) << endl;
+
+    vector<int> num = {0,1,2,-3,4,5,-6,7};
+    cout << "max sum: " << maxsum.maxsum_subarray(num) << endl;
     return 0;
 }
